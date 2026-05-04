32cca188c053be4ab6759517d7c8efd7a49141e7
Avaray <1459553+Avaray@users.noreply.github.com>
2026-05-04 16:10:21 +0200

    fix(java): remove public modifier from IpChecker class to avoid file naming error

--- dist/gip.java
+++ dist/gip.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
-public class IpChecker {
+class IpChecker {
 
     private static final String[] urls = {
   "http://eth0.me",
